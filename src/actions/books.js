da255b92f66236cc952ee2d3c90585a50ee45303
Brajesh Jaishwal <brajesh.jaishwal@gmail.com>
2018-10-22 10:56:01 +0530

    books store

---
+++ src/actions/books.js
@@ -0,0 +1,28 @@
+import uuid from 'uuid';
+
+export const addBook = ({
+    title = '',
+    description = '',
+    author = '',
+    published = 0
+} = {}) => ({
+    type: 'ADD_BOOK',
+    book: {
+        id: uuid(),
+        title,
+        description,
+        author,
+        published
+    }
+});
+
+export const removeBook = ({ id } = {}) => ({
+    type: 'REMOVE_BOOK',
+    id
+});
+
+export const editBook = (id, updates) => ({
+    type: 'EDIT_BOOK',
+    id,
+    updates
+});
